e8d15b99e97509902967ae4f05a964ea6e43d5ea
Manh Duc <ducmanh200555@gmail.com>
2025-09-11 20:41:24 +0700

    11/9

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let a = Math.random();
-let b = 10 * a;
-console.log(b);
+let tong = Number(prompt("Nhập giá trị của tổng x và y:"));
+let hieu = Number(prompt("Nhập giá trị hiệu của x và y: "));
+let x = (tong + hieu) / 2,
+  y = (tong - hieu) / 2;
+console.log(`Giá trị của x là: ${x}`);
+console.log(`Giá trị của y là: ${y}`);
