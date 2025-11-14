42a4a5f9e5a6aa11244701e501439503a68922d1
Manh Duc <ducmanh200555@gmail.com>
2025-11-14 22:38:34 +0700

    thu thach

--- hehe.js
+++ hehe.js
@@ -1,4 +1,17 @@
-const menu = document.getElementById("menu");
+"use strict";
+// const p = new Promise((resolve, reject) => {
+//   // thực hiện hành động nào đó
+//   const success = true;
+//   if (success) {
+//     resolve("Hoàn thành!"); // khi thành công
+//   } else {
+//     reject("Thất bại!"); // khi lỗi
+//   }
+// });
+const p = new Promise((resolve, error) => {
+  error("That bai");
+});
 
-console.log(menu);
-console.log(menu.childNodes);
+p.catch((p) => {
+  console.error(p);
+});
